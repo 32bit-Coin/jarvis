60c00c6bcdc8484c4f0797c5bf1db5fcd59c973b
Mars <mars@Yuri-1.2>
2021-10-28 23:05:08 +0200

    test change

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Main() {
     return (
         <div className="jarvis-main">
-            <h1> MAIN</h1>
+            <h1> MAIN j</h1>
         </div>
     )
 }
