269841795fcef1d061ed5252667271a9bd15a8a9
Mars <mars@Yuri-1.2>
2021-10-31 23:18:54 +0100

    jsp

--- src/components/Tania.js
+++ src/components/Tania.js
@@ -1,32 +1,55 @@
 import React, { useState } from 'react';
 
+const TableBody = () => {
+    return (
+        <tbody>
+            <tr>
+                <th>Events</th>
+                <th>Events</th>
+                <th>Events</th>
+                <th>Events</th>
+                <th>Events</th>
+                <th>Events</th>
+                <th>Events</th>
+            </tr>
+        </tbody>
+  }
+
 class Tania extends React.Component {
     render() {
         return (
             <div className="Tania">
+                <h1>Display</h1>
                 <table>
                     <thead>
                         <tr>
-                            <th>Name</th>
-                            <th>Job</th>
+                            <th>Monday</th>
+                            <th>Tuesday</th>
+                            <th>Wedneday</th>
+                            <th>Thursday</th>
+                            <th>Friday</th>
+                            <th>Saturday</th>
+                            <th>Sunday</th>
+                        </tr>
+                        <tr>
+                            <th>Date</th>
+                            <th>Date</th>
+                            <th>Date</th>
+                            <th>Date</th>
+                            <th>Date</th>
+                            <th>Date</th>
+                            <th>Date</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>Charlie</td>
-                            <td>Janitor</td>
-                        </tr>
-                        <tr>
-                            <td>Mac</td>
-                            <td>Bouncer</td>
-                        </tr>
-                        <tr>
-                            <td>Dee</td>
-                            <td>Aspiring actress</td>
-                        </tr>
-                        <tr>
-                            <td>Dennis</td>
-                            <td>Bartender</td>
+                            <th>Events</th>
+                            <th>Events</th>
+                            <th>Events</th>
+                            <th>Events</th>
+                            <th>Events</th>
+                            <th>Events</th>
+                            <th>Events</th>
                         </tr>
                     </tbody>
                 </table>
